bfabdcdf3a86960f2ffd8969d7dc8e8ac2680563
e.golubcov <evgenii.golubcov@iqoption.com>
2020-05-05 18:43:55 +0300

    refactoring

--- src/test/java/YandexMarketTest.java
+++ src/test/java/YandexMarketTest.java
@@ -1,12 +1,12 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -46,7 +46,7 @@ public class YandexMarketTest {
         addToCompare();
         checkNotification( "Всего в списке 2 товара из категории Мобильные телефоны");
         closeNotification();
-        
+
         openCompareMarket();
         checkCountCompare(2);
         clickAllParameters();
